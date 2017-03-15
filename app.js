c81cb936e5aaf5c71f31707fda67bc3b05ae8ed3
Jens H. Nielsen <jens@jens.hk>
2017-03-15 15:03:41 +0800

    Google Login

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Setup local strategy
-require('./passport/twitter')(passport);
+//require('./passport/twitter')(passport);
 require('./passport/google')(passport);
 
 // Setup routes
--- passport/google.js
+++ passport/google.js
@@ -1,7 +1,7 @@
-var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-var appid = '';
-var appSecret = '';
+var appid = '319045524874-g576nb8igfpj5ma41fdl9g5ldoc936n0.apps.googleusercontent.com';
+var appSecret = 'M_Ci4bTixvNifnnC-PKwEa5h';
 var callback = 'http://localhost:3000/auth/google/callback';
 
 var User = require('../models/user');
@@ -9,10 +9,9 @@ var User = require('../models/user');
 module.exports = function (passport) {
     
     passport.use("google", new GoogleStrategy({
-        consumerKey: appid,
-	    consumerSecret: appSecret,
-	    callbackURL: callback,
-        profileFields: ['id', 'emails', 'username', 'link']
+        clientID: appid,
+        clientSecret: appSecret,
+	    callbackURL: callback
     },
 
     function(token, tokenSecret, profile, done) {
